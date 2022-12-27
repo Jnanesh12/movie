0b288208dc485b7b913e56385ee5734fa3696d29
Jnanesh12 <108192234+Jnanesh12@users.noreply.github.com>
2022-12-27 20:59:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jnanesh124/newhop0 /AutoAnura
+  git clone https://github.com/Jnanesh12/movie /AutoAnura
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoAnura
